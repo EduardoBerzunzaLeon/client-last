59372f8b6c87e400470b5ea2f1306911cc58fb49
unknown <eduardoberzunzal@gmail.com>
2022-03-29 13:11:17 -0600

    Test Divider component

---
+++ src/tests/components/divider/Divider.test.tsx
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import { Divider } from '../../../components/Divider/Divider';
+
+describe('<Divider />', () => {
+  test('should match to snapshot', () => {
+    const wrapper = shallow(<Divider text="Name" icon="user" />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('i').prop('className')).toContain('pi-user');
+    expect(wrapper.find('b').text().trim()).toContain('Name');
+  });
+});
