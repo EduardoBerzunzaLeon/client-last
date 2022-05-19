322bd54e7d5c2f7cd36e080c9b2669af51fd043c
unknown <eduardoberzunzal@gmail.com>
2022-05-19 12:38:26 -0500

    Implemented excel download button

--- src/screens/admin/subjects/components/ExcelButton.tsx
+++ src/screens/admin/subjects/components/ExcelButton.tsx
@@ -1,39 +1,49 @@
 import { Button } from 'primereact/button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 import { useGetSubjectsForExcelQuery } from '../../../../redux/subject/subject.api';
 
 export const ExcelButton = () => {
   const [ skip, setSkip ] = useState(true);
-  const { data, isLoading } = useGetSubjectsForExcelQuery(null, { skip });
+  const [ isLoading, setIsLoading ] = useState(false);
+  const { data } = useGetSubjectsForExcelQuery(null, { skip });
 
-  console.log({ data, isLoading });
+  useEffect(() => {
+    console.log({ data, isLoading });
+    if (data && isLoading) {
+      // setIsLoading(true);
+      // eslint-disable-next-line no-use-before-define
+      exportExcel();
+      // exportExcel();
+    }
+  }, [ data, isLoading ]);
 
   const saveAsExcelFile = (buffer: any, fileName: string) => {
-    import('file-saver').then((module) => {
-      if (module && module.default) {
-        const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-        const EXCEL_EXTENSION = '.xlsx';
-        const dataset = new Blob([ buffer ], {
-          type: EXCEL_TYPE,
-        });
-
-        module.default.saveAs(dataset, `${fileName}_export_${new Date().getTime()}${EXCEL_EXTENSION}`);
-      }
+    const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const EXCEL_EXTENSION = '.xlsx';
+    const dataset = new Blob([ buffer ], {
+      type: EXCEL_TYPE,
     });
+
+    saveAs(dataset, `${fileName}_export_${new Date().getTime()}${EXCEL_EXTENSION}`);
+    setIsLoading(false);
   };
   const exportExcel = () => {
-    if (data) {
-      const worksheet = XLSX.utils.json_to_sheet(data.data);
-      const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: [ 'data' ]};
-      const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-      saveAsExcelFile(excelBuffer, 'products');
-    }
+    const header = [[ 'ID', 'Nombre', 'Semestre', 'Deprecado', 'Materias Requeridas', 'Materias Correlativas', 'Creditos', 'Horas Practicas', 'Horas Teóricas', 'Horas Totales', ' Núcleo Academico' ]];
+    const worksheet = XLSX.utils.json_to_sheet(data!.data);
+    XLSX.utils.sheet_add_aoa(worksheet, header, { origin: 'A1' });
+
+    const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: [ 'data' ]};
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    saveAsExcelFile(excelBuffer, 'materias');
   };
 
   const handleClick = () => {
-    setSkip(false);
-    exportExcel();
+    if (skip) {
+      setSkip(false);
+    }
+    setIsLoading(true);
   };
 
   return (
@@ -42,6 +52,7 @@ export const ExcelButton = () => {
       icon="pi pi-file-excel"
       label="Exportar Excel"
       className="p-button p-button-success m-2"
+      loading={isLoading}
       onClick={handleClick}
     />
   );
