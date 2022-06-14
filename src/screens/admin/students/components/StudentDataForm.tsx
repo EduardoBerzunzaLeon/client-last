29a6233191200cfc2ade6d2f730f4d5095da1b8c
unknown <eduardoberzunzal@gmail.com>
2022-06-14 15:05:13 -0500

    Prepare refactor to virtual scroll professors

--- src/screens/admin/students/components/StudentDataForm.tsx
+++ src/screens/admin/students/components/StudentDataForm.tsx
@@ -1,71 +1,113 @@
 import { AutoComplete } from 'primereact/autocomplete';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextImageBody } from '../../../../components/datatable';
 
 import { StudentResume } from '../../../../interfaces/api';
-import { useGetProfessorsQuery } from '../../../../redux/professor/professor.api';
+import { useGetProfessorsByFullNameQuery, useGetProfessorsQuery } from '../../../../redux/professor/professor.api';
 
 interface Props {
     student: StudentResume | undefined;
 }
-// const items = Array.from({ length: 100000 }).map((_, i) => ({ label: `Item #${i}`, value: i }));
+
+interface ProfessorItem {
+  fullname: string,
+  value: string,
+  avatar: string
+}
+
+const prepareData = <T extends ProfessorItem>(data: T[]) => data.map((p) => ({
+  fullname: p.fullname,
+  value: p.id,
+  avatar: p.avatar,
+}));
 
 export const StudentDataForm = ({ student }: Props) => {
   console.log(student);
   const [ selectedItem, setSelectedItem ] = useState<any>(null);
   const [ filteredItems, setFilteredItems ] = useState<any>(null);
-  //   const [ query, setQuery ] = useState<string>('');
+  const [ query, setQuery ] = useState<string>('');
 
   const {
     data,
-  } = useGetProfessorsQuery('professors');
+    isLoading,
+  } = useGetProfessorsQuery('professors', { skip: !!query });
 
-  //   const {
-  //     data: professors,
-  //   } = useGetProfessorsByFullNameQuery(query, { skip: !!query });
+  const {
+    data: professors,
+    isLoading: isLoadingSpecific,
+  } = useGetProfessorsByFullNameQuery(query, { skip: !query });
 
-  console.log({ filteredItems });
-  //   useEffect(() => {
-  //     setFilteredItems(professors?.data);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   }, [ professors ]);
+  const itemTemplate = (item: any) => (
+    <TextImageBody text={item.fullname} imageURL={item.avatar} />
+  );
 
-  const itemTemplate = (item: any) => {
-    console.log({ item });
-    return (
-      <TextImageBody text={item.fullname} imageURL={item.avatar} />
-    );
-  };
-
-  const searchItems = (event: any) => {
-    //   setQuery(event.query);
-
-    console.log(event);
+  useEffect(() => {
     let dataPrepare: any[] = [];
-    if (data?.data) {
-      dataPrepare = data.data.map((professor) => ({
-        fullname: professor.fullname,
-        value: professor.id,
-        avatar: professor.avatar,
+    if (query && !isLoadingSpecific) {
+      if (professors?.data) {
+        dataPrepare = professors.data.map((p) => ({
+          fullname: p.fullname,
+          value: p.id,
+          avatar: p.avatar,
+        }));
+      }
+    } else if (data?.data && !isLoading) {
+      dataPrepare = data.data.map((p) => ({
+        fullname: p.fullname,
+        value: p.id,
+        avatar: p.avatar,
       }));
     }
     setFilteredItems(dataPrepare);
+  }, [ query, isLoading, isLoadingSpecific, data, professors ]);
+
+  const searchItems = (event: any) => {
+    if (query === '' && event.query === '') {
+      let dataPrepare: any[] = [];
+
+      if (data) {
+        dataPrepare = data?.data.map((p) => ({
+          fullname: p.fullname,
+          value: p.id,
+          avatar: p.avatar,
+        }));
+      }
+      setFilteredItems(dataPrepare);
+    } else {
+      setQuery(event.query.trim());
+    }
+  };
+
+  const onHide = () => {
+    setQuery('');
   };
 
   return (
     <div>
       <h5>Virtual Scroll (100000 Items)</h5>
-      <AutoComplete
-        value={selectedItem}
-        suggestions={filteredItems}
-        completeMethod={searchItems}
-        virtualScrollerOptions={{ itemSize: 38 }}
-        field="fullname"
-        dropdown
-        itemTemplate={itemTemplate}
-        onChange={(e) => setSelectedItem(e.value)}
-      />
+      <div className="field pt-2">
+        <AutoComplete
+          value={selectedItem}
+          suggestions={filteredItems}
+          completeMethod={searchItems}
+          virtualScrollerOptions={{
+            itemSize: 38,
+            showLoader: true,
+            delay: 250,
+          }}
+          field="fullname"
+          dropdown
+          itemTemplate={itemTemplate}
+          onChange={(e) => setSelectedItem(e.value)}
+          onHide={onHide}
+          className="w-full"
+          forceSelection
+          panelClassName="overflow-hidden"
+        />
+      </div>
+
     </div>
+
   );
 };
 
